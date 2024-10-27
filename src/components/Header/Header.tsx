6b2e940d7989371edb917090c9ad1089b6154095
Damir <sagalbaevdamir098@gmail.com>
2024-10-27 14:18:08 +0600

    add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+import logoMarvel from '../../assets/logoMarvel.png'
+function Header() {
+    return (
+        <header className={classes.header}>
+            <div className={classes.container}>
+                <img src={logoMarvel} className={classes.image} alt="logo-marverl" />
+                <div className={classes.menu_container}>
+                    <NavLink 
+                        to="/characters" 
+                        className={({ isActive }) => `${classes.menu} ${isActive ? classes.active : ''}`}
+                    >
+                        <span>Characters</span>
+                    </NavLink>
+                    <NavLink 
+                        to="/comics" 
+                        className={({ isActive }) => `${classes.menu} ${isActive ? classes.active : ''}`}
+                    >
+                        <span>Comics</span>
+                    </NavLink>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
