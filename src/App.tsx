3afddb90e5724269a55fe62468d79954ed25e66f
Damir <sagalbaevdamir098@gmail.com>
2024-10-28 19:59:57 +0600

    edit app and add logic with navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import './App.css'
-import Header from './components/Header/Header'
+import Characters from './pages/Characters/Characters'
+import Comics from './pages/Comics/Comics';
+import Layout from './Layout';
+import { BrowserRouter, Navigate } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import CharacterDetail from './pages/CharacterDetail/CharacterDetail';
+import ComicDetail from './pages/ComicDetail/ComicDetail';
+function AppRoutes() {
+  const routes = [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Navigate to="/characters" replace /> },
+        { path: "characters", element: <Characters /> },
+        { path: "comics", element: <Comics /> },
+        { path: "characters/:id", element: <CharacterDetail /> },
+        { path: "comics/:id", element: <ComicDetail /> },
+      ],
+    },
+  ];
 
-function App() {
-
-  return (
-    <>
-      <Header/>
-    </>
-  )
+  return useRoutes(routes);
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
