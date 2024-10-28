f4fcec24915a777e293c20d8e9e7553977f10b5f
Damir <sagalbaevdamir098@gmail.com>
2024-10-28 19:54:21 +0600

    add interfaces for constans

---
+++ src/components/interface/ICharacter.tsx
@@ -0,0 +1,8 @@
+interface ICharacter {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    comics:string[];
+}
+export default ICharacter;
---
+++ src/components/interface/IComics.tsx
@@ -0,0 +1,8 @@
+interface IComics {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    characters:string[];
+}
+export default IComics;
