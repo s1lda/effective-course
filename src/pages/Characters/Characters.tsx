966287addd43689f94238338aea87427ae90cb4f
Damir <sagalbaevdamir098@gmail.com>
2024-11-07 20:15:53 +0600

    upd Characters and css

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -1,29 +1,56 @@
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import classes from './Characters.module.css';
-import charactersData from '../../constans/CharactersData';
 import ItemCard from '../../components/ItemCard/ItemCard';
-function Characters() {
+import CharactersStore from '../../stores/CharactersStore';
+import Pagination from '../../components/Pagination/Pagination';
+const Characters = observer(() => {
+    useEffect(() => {
+        CharactersStore.fetchCharacters(0);
+    }, []);
+
+    const handlePageChange = (page: number) => {
+        const newOffset = (page - 1) * CharactersStore.limit;
+        CharactersStore.setOffset(newOffset);
+    };
+
     return (
         <section className={classes.characters}>
             <div className={classes.container}>
                 <div className={classes.title}>
-                    <p>Characters ({charactersData.length})</p>
+                    <p>Characters ({CharactersStore.totalItems})</p>
                 </div>
                 <div className={classes.search_container}>
                     <input
                         type="text"
                         placeholder="Введите имя персонажа..."
                         className={classes.search_input}
+                        value={CharactersStore.searchTerm}
+                        onChange={(e) => CharactersStore.setSearchTerm(e.target.value)}
                     />
-                    <button className={classes.search_button}>Поиск</button>
+                    <button className={classes.search_button} onClick={() => CharactersStore.triggerSearch()}>Search</button>
                 </div>
+
                 <div className={classes.characters_items}>
-                    {charactersData.map((character, index) => (
-                        <ItemCard key={index} {...character} type='characters' />
-                    ))}
+                    {CharactersStore.loading ? (
+                        <p className={classes.loading}>Загрузка...</p>
+                    ) : CharactersStore.filteredCharacters.length > 0 ? (
+                        CharactersStore.filteredCharacters.map((character) => (
+                            <ItemCard key={character.id} {...character} type="characters" />
+                        ))
+                    ) : (
+                        <p>Персонажи не найдены</p>
+                    )}
                 </div>
+                <Pagination
+                    offset={CharactersStore.offset}
+                    limit={CharactersStore.limit}
+                    totalItems={CharactersStore.totalItems}
+                    onPageChange={handlePageChange}
+                />
             </div>
         </section>
     );
-}
+});
 
 export default Characters;
