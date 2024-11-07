2b651833dbc60bf5f97ffae800ce3b3c0852c4db
Damir <sagalbaevdamir098@gmail.com>
2024-11-07 20:16:17 +0600

    upd CharacterDetail

--- src/pages/CharacterDetail/CharacterDetail.tsx
+++ src/pages/CharacterDetail/CharacterDetail.tsx
@@ -1,38 +1,41 @@
+import { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import charactersData from '../../constans/CharactersData'; 
-import comicsData from '../../constans/ComicsData'; 
-import classes from './CharacterDetail.module.css'; 
+import { observer } from 'mobx-react-lite';
+import characterIdStore from '../../stores/CharacterIdStore';
+import classes from './CharacterDetail.module.css';
 
-const CharacterDetail = () => {
-    const { id } = useParams<{ id: string }>();
-    const character = charactersData.find(ch => ch.id === id);
+const CharacterDetail = observer(() => {
+  const { id } = useParams<{ id: string }>();
 
-    if (!character) {
-        return <div>Character not found</div>;
+  useEffect(() => {
+    if (id) {
+      characterIdStore.fetchCharacter(id);
     }
+  }, [id]);
 
-    return (
-        <div className={classes.characterDetail}>
-            <h1 className={classes.characterName}>{character.name}</h1>
-            <div className={classes.imageWrapper}>
-            <img className={classes.characterImage} src={character.image} alt={character.name} />
-            </div>
-            <p className={classes.characterDescription}>{character.description}</p>
-            <h2 className={classes.comicListName}>Комиксы</h2>
-            <ul className={classes.comicList}>
-                {character.comics.map(comicId => {
-                    const comic = comicsData.find(c => c.id === comicId);
-                    return (
-                        comic && (
-                            <li key={comicId}>
-                                <Link to={`/comics/${comic.id}`}>{comic.name}</Link>
-                            </li>
-                        )
-                    );
-                })}
-            </ul>
-        </div>
-    );
-};
+  const { character, loading, error } = characterIdStore;
+
+  if (loading) return <div>Загрузка...</div>;
+  if (error) return <div>{error}</div>;
+  if (!character) return <div>Персонаж не найден</div>;
+
+  return (
+    <div className={classes.characterDetail}>
+      <h1 className={classes.characterName}>{character.name}</h1>
+      <div className={classes.imageWrapper}>
+        <img className={classes.characterImage} src={character.image} alt={character.name} />
+      </div>
+      <p className={classes.characterDescription}>{character.description}</p>
+      <h2 className={classes.comicListName}>Комиксы</h2>
+      <ul className={classes.comicList}>
+        {character.comics.map((comic) => (
+          <li key={comic.id}>
+            <Link to={`/comics/${comic.id}`}>{comic.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
 
 export default CharacterDetail;
